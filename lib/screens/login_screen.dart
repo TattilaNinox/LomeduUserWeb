577987c4d234107d49a1ebadc7fd48b9c5f914a0
Attila Tölgyesi <tattila.ninox@gmail.com>
2025-10-06 19:59:08 +0200

    Login screen: logo adjustments (size 180px, spacing 8px) & updated PNG

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 import '../utils/device_fingerprint.dart';
 
@@ -161,6 +162,12 @@ class LoginScreenState extends State<LoginScreen> {
                   mainAxisSize: MainAxisSize.min,
                   children: [
                     // Cím
+                    Image.asset(
+                      'assets/images/login_LOGO.png',
+                      height: 180,
+                      fit: BoxFit.contain,
+                    ),
+                    const SizedBox(height: 8),
                     const Text(
                       'Lomedu Belépés',
                       style: TextStyle(
