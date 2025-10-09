c216745657291db7b4538dab5a72fcba49c7d0ea
Attila Tölgyesi <tattila.ninox@gmail.com>
2025-10-09 16:10:16 +0200

    Remove Next.js related files and configs; clean up repository for pure Flutter project

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <a className="text-blue-700 underline" href="/login">Tovább a bejelentkezéshez</a>
-    </main>
-  );
-}
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./html_src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-} 
